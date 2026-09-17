8cacecf18ec722dfc17e4dd794da7870706d35e6
nguyencanhthanh308-ui <nguyencanhthanh308@gmail.com>
2026-09-17 19:39:06 +0700

    component fix

--- zpsr0019/webapp/Component.js
+++ zpsr0019/webapp/Component.js
@@ -1,9 +1,15 @@
+/**
+ * eslint-disable @sap/ui5-jsdocs/no-jsdoc
+ */
+/**
+ * Loading config xlsx unpackage.
+ */
 sap.ui.loader.config({
 	paths: {
-		"zpsr0019/libs/xlsx": "https://unpkg.com/dep-xlsx-js-style@1.2.7/dist/xlsx.min",
+		"zpsr0009/libs/xlsx": "https://unpkg.com/dep-xlsx-js-style@1.2.7/dist/xlsx.min",
 	},
 	shim: {
-		"zpsr0019/libs/xlsx": {
+		"zpsr0009/libs/xlsx": {
 			amd: true,
 			exports: "XLSX",
 		},
@@ -11,27 +17,34 @@ sap.ui.loader.config({
 	async: true,
 });
 
-sap.ui.define(["sap/ui/core/UIComponent", "zpsr0019/model/models"], (UIComponent, models) => {
-	"use strict";
+sap.ui.define(
+	["sap/ui/core/UIComponent", "sap/ui/Device", "zpsr0009/model/models"],
+	function (UIComponent, Device, models) {
+		"use strict";
 
-	return UIComponent.extend("zpsr0019.Component", {
-		metadata: {
-			manifest: "json",
-			interfaces: ["sap.ui.core.IAsyncContentCreation"],
-			config: {
-				fullWidth: true,
+		return UIComponent.extend("zpsr0009.Component", {
+			metadata: {
+				manifest: "json",
+				config: {
+					fullWidth: true,
+				},
 			},
-		},
 
-		init() {
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
+			/**
+			 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+			 * @public
+			 * @override
+			 */
+			init: function () {
+				// call the base component's init function
+				UIComponent.prototype.init.apply(this, arguments);
 
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
+				// enable routing
+				this.getRouter().initialize();
 
-			// enable routing
-			this.getRouter().initialize();
-		},
-	});
-});
+				// set the device model
+				this.setModel(models.createDeviceModel(), "device");
+			},
+		});
+	}
+);
